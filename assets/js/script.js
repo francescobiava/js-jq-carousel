aaaf119f367e785a686e219508f2578ad3b9cc4c
Francesco <franc.ns@gmail.com>
2019-11-07 16:17:24 +0100

    added carousel with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
   
+  $('.next').click(nextSlide);
+  $('.prev').click(prevSlide);
+
 });
+
+// functions
+function nextSlide() {
+  
+  var imgPosition = $('.carousel .images img.active');
+  var navPosition = $('.carousel .nav i.active');
+
+  imgPosition.removeClass('active');
+  navPosition.removeClass('active');
+
+  if (imgPosition.hasClass('last')) {
+    $('.carousel .images img.first').addClass('active');
+    $('.carousel .nav i.first').addClass('active');
+  } else {
+    imgPosition.next('img').addClass('active');
+    navPosition.next('i').addClass('active');
+  }
+}
+
+function prevSlide() {
+  
+  var imgPosition = $('.carousel .images img.active');
+  var navPosition = $('.carousel .nav i.active');
+
+  imgPosition.removeClass('active');
+  navPosition.removeClass('active');
+
+  if (imgPosition.hasClass('first')) {
+    $('.carousel .images img.last').addClass('active');
+    $('.carousel .nav i.last').addClass('active');
+  } else {
+    imgPosition.prev('img').addClass('active');
+    navPosition.prev('i').addClass('active');
+  }
+}
